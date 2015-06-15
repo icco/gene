a4ff3466ad1e3878b48baa1704f1a17b77379293
Nat Welch <nat@natwelch.com>
2015-06-15 17:52:54 +0000

    better config

--- Gemfile
+++ Gemfile
@@ -1,11 +1,16 @@
-# If you do not have OpenSSL installed, update
-# the following line to use "http://" instead
-source 'https://rubygems.org'
+source "https://rubygems.org"
 
-gem "middleman", "~>3.3.12"
+gem "middleman", "~> 3.3.12"
 
-# Live-reloading plugin
-gem "middleman-livereload", "~> 3.1.0"
+# For shrinking things
+gem "middleman-imageoptim"
+gem "middleman-simple-thumbnailer"
+
+# For deploying to gh-pages
+gem "middleman-deploy", "~> 1.0"
+
+# Faster json
+gem "oj"
 
 # For faster file watcher updates on Windows:
 gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
--- config.rb
+++ config.rb
@@ -3,9 +3,9 @@
 ###
 
 # Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
+compass_config do |config|
+  config.output_style = :compact
+end
 
 ###
 # Page options, layouts, aliases and proxies
@@ -14,31 +14,16 @@
 # Per-page layout changes:
 #
 # With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
+page "/index.html", :layout => false
 
 ###
 # Helpers
 ###
 
+activate :middleman_simple_thumbnailer
+
 # Automatic image dimensions on image_tag helper
 # activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -56,17 +41,29 @@ set :images_dir, 'img'
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
-  # activate :asset_hash
+  activate :asset_hash
+
+  # https://github.com/plasticine/middleman-imageoptim
+  activate :imageoptim do |options|
+    options.pngout = false
+    options.svgo = false
+  end
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
+end
+
+activate :deploy do |deploy|
+  deploy.method = :git
+  deploy.branch = "gh-pages"
+  deploy.build_before = true
 end
